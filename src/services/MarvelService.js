bbfcf7ba5419fcf91a1f3d5e41a77a56e0a49abe
Rigina <papakhina.rigina@yandex.ru>
2022-06-24 11:15:32 +0300

    first commit

---
+++ src/services/MarvelService.js
@@ -0,0 +1,21 @@
+class MarvelService{
+  _apiBase='https://gateway.marvel.com:443/v1/public/';
+    _apiKey='apikey=fae5495e80fe2fc6cdf6d17d0634f7dd';
+  getResource = async (url) => {
+    let res = await fetch(url);
+
+    if (!res.ok) {
+        throw new Error(`Could not fetch ${url}, status: ${res.status}`);
+    }
+
+    return await res.json();
+}
+
+getAllCharacters =()=> {
+  return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+}
+getCharacter =(id)=> {
+  return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+}
+}
+export default MarvelService;
