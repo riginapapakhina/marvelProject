a7db6a5fca204556cc9a9373de594fbd736e36ab
Rigina <papakhina.rigina@yandex.ru>
2022-06-27 16:21:26 +0300

    character descriptions added

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -21,11 +21,13 @@ getCharacter = async(id)=> {
 }
 _transformCharacter =(char) => {
   return {
+    id:char.id,
     name:char.name,
     description:char.description ? `${char.description.slice(0,210)}...` : 'Ooops, no info here yet, sorry...I bet this character is amazing. How about you Google it?',
     thumbnail:char.thumbnail.path + '.' + char.thumbnail.extension,
     homepage:char.urls[0].url,
-    wiki:char.urls[1].url
+    wiki:char.urls[1].url,
+    comics:char.comics.items
   }
 }
 }
